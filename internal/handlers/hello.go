d339e263e2e1e05d8b52d5028568880d7ff16850
Aleksei Gerasev <1999bag@gmail.com>
2025-12-05 21:05:18 +0300

    refact: improve hello handler

---
+++ internal/handlers/hello.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+type HelloResponse struct {
+	Message   string    `json:"message"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	hello := HelloResponse{"hello from prod-ready service", time.Now()}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(hello); err != nil {
+		slog.Debug("client disconnected during response", "error", err)
+	}
+}
